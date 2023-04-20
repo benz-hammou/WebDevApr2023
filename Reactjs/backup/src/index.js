a9db963df8c6d2e8a689894ec9103c84040a1df6
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-04-20 15:23:09 +0200

    employee exe

--- Reactjs/backup/src/index.js
+++ Reactjs/backup/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style/style.css'
-import App from './app';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/style.css'
 
